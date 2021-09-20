fe80d62d2baaae21809643ed2fd2f58668a37264
hyunlee214 <hyun110214@naver.com>
2021-09-21 00:50:09 +0900

    migration파일 수정 후 서버 실행 -> DB반영

--- migrations/20210920153927-create-user.js
+++ migrations/20210920153927-create-user.js
@@ -1,21 +1,22 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
+    return await queryInterface.createTable('users', {
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true
+        },
+        primaryKey: true
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       salt: {
         type: Sequelize.STRING
@@ -31,6 +32,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('users');
+    return await queryInterface.dropTable('users');
   }
 };
