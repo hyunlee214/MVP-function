b8eb378a29bbdf57a1f124b678b6a5282d0b329d
hyunlee214 <hyun110214@naver.com>
2021-09-22 15:57:47 +0900

    회원가입 페이지 redirect 완료

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const models = require('../models');
 const crypto = require('crypto');
 
 
@@ -7,5 +8,25 @@ const crypto = require('crypto');
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/sign_up', function(req, res, next) {
+  res.render('user/signup');
+});
+
+router.post("/sign_up", function(req, res, next){
+  let body = req.body;
+
+  models.user.create({
+    name: body.userName,
+    email: body.userEmail,
+    password: body.password
+  })
+  .then( result => {
+    res.redirect("/users/sign_up");
+  })
+  .catch( err => {
+    console.log(err)
+  })
+})
 
 module.exports = router;
