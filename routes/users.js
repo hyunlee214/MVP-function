df96811600864d78a0cdeba534d9bab3da88b3ae
hyunlee214 <hyun110214@naver.com>
2021-09-24 01:54:59 +0900

    로그인 처리 부분 일부 수정

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,6 @@ const router      = express.Router();
 const models      = require('../models');
 const crypto      = require('crypto');
 
-router.get('/', function(req, res, next) {
-  res.send('main page');
-})
 
 router.get('/sign_up', function(req, res, next) {
   res.render("users/signup");
@@ -27,25 +24,35 @@ router.post('/sign_up', function(req, res, next) {
   res.redirect('/users/sign_up');
 })
 
+router.get('/', function(req, res, next) {
+  res.send('main page');
+});
+
 
 router.get('/login', function(req, res, next) {
-  res.render('users/login');
+  res.render('users/login');s
 });
 
 router.post('/login', function(req, res, next) {
   let body = req.body;
 
-  let inputPassword = body.password;
-  let salt = Math.round((new Date().valueOf() * Math.random())) + "";
-  let hashPassword = crypto.createHash('sha512').update(inputPassword + salt).digest('hex');
+  let result = models.user.findOne({
+    where: {
+      email: body.userEmail
+    }
+  });
+
   let dbPassword = result.dataVaules.password;
+  let inputPassword = body.password;
+  let salt = result.dataVaules.salt;
+  let hashPassword = crypto.createHash('sha512').update(inputPassword + salt).digest('hex');
 
   if (dbPassword === hashPassword) {
     res.redirect('/users');
   }
   else {
-    res.redirect('/users/login')
+    res.redirect('/users/login');
   }
-})
+});
 
 module.exports = router;
