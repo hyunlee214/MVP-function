6a542b6118892aa8f5afd7fdd8c5a82fd2f35cd3
hyunlee214 <hyun110214@naver.com>
2021-09-30 09:13:25 +0900

    img파일업로드기능 추가

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const { route } = require('.');
 const router = express.Router();
 const multer = express('multer');
+
+let stroage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploadFile/')
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ stroage: stroage});
 
 // http://localhost:3000/fileUpload/uploadPage
 router.get('/uploadPage', (req, res) => {
   res.render('upload');         // ejs파일
+})
+
+router.post('/uploadComplete', upload.single('imgFile'), (req, res) => {
+  let file = req.file
+  let result = {
+    originalName : file.originalName,
+  }
+  res.json(result);
 })
 
 module.exports = router;
